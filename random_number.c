7ca8975b8cd15d23568a338fcd6e2ad8467bda94
Sakshi Bankar_092 <129163710+SakshiBankar1803@users.noreply.github.com>
2023-07-22 21:39:02 +0530

    Add files via upload

---
+++ random_number.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int search(int a[30],int num,int n)
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+    if(a[i]==num)
+    return i;
+  }
+  return i;
+}
+
+
+
+int main()
+{
+  int a[30],i,n,num;
+  printf("enter limit::");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  a[i]=rand()%1000;
+  
+  printf("random numbers::");
+  for(i=0;i<n;i++)
+  printf("%d\t",a[i]);
+  
+  print("enter number to search::");
+  scanf("%d",&num);
+  
+  if(search(a,num,n)==n)
+   printf("number not found::");
+   else
+   printf("number is found");
+   }
+  
