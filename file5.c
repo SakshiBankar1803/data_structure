0bac57999241ff3a6e6b35f81506bf4373f6d11a
Sakshi Bankar_092 <129163710+SakshiBankar1803@users.noreply.github.com>
2023-07-26 19:02:20 +0530

    Update file5.c

--- file5.c
+++ file5.c
@@ -3,32 +3,47 @@
 int main()
 {
   FILE *fp;
-  int num[20],n,f=0,top,mid,bottom;
-  fp=fopen("numbers.txt","r");
+  int n,num,f=0,number,i=0,a[30],ans;
+  int file(int a[30],int n,int number);
+  fp=fopen("num.txt","r");
   if(fp==NULL)
   {
     printf("file not found");
     exit(0);
   }
-  printf("enter number to search:");
-  scanf("%d",&n);
-  top=0;
-  bottom=n-1;
   while(!feof(fp))
-  { fscanf(fp,"%d",&num);
-    mid=(top+bottom)/2;
-    if(num[mid]==n)
-    {
-      f=1;break;
-    }
-    if(n>num[mid])
-     top=mid+1;
-     else
-     bottom=mid-1;
+  {
+    fscanf(fp,"%d",&num);
+    a[i++]=num;
   }
-  if(f==1)
+  n=i;
+  printf("enter number to search::");
+  scanf("%d",&number);
+  ans=file(a,n,number);
+  if(ans==1)
   printf("number found");
   else
-  printf("number not found");
+  printf("not found");
   fclose(fp);
+  }
+  
+  int file(int a[30],int n,int number)
+  { int top,mid,bottom,f=0;
+  top=0;
+  bottom=n-1;
+  while(top<=bottom)
+  {
+    mid=(top+bottom)/2;
+    if(number==a[mid])
+    {
+      f=1;
+      break;
+    }
+    if(number>a[mid])
+    top=mid+1;
+    else
+    bottom=mid-1;
+  }
+   return f;
+  
 }
